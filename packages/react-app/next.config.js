d36261a1769837b9011af1c56512152227766630
Glory Agatevure <agatevureglory@gmail.com>
2023-02-10 01:25:00 +0100

    modified nextconfig

--- packages/react-app/next.config.js
+++ packages/react-app/next.config.js
@@ -1,13 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false
-    }
-    return config
-  }
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   webpack: (config) => {
+//     config.resolve.fallback = {
+//       fs: false
+//     }
+//     return config
+//   }
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/Dashboard': { page: '/Dashboard' },
+      '/CreateAccount': { page: '/CreateAccount' },
+      '/Support': { page: '/Support' },
+      '/Withdraw': { page: '/Withdraw' },
+    }
+  },
+}
